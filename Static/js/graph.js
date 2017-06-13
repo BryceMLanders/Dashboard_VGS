53a72775a7fdc0733c8c158045709d28f586d21e
Bryce <Brycemlanders@gmail.com>
2017-06-13 12:12:41 +0100

    Added more Graphs

--- Static/js/graph.js
+++ Static/js/graph.js
@@ -26,38 +26,75 @@ function makeGraphs(error, projectsJson) {
         return d["Name"]
    });
 
- 
+   var genreDim = ndx.dimension(function (d) {
+       return d["Genre"]
+   })
+
+   var ninsalesbyYear = yearDim.group().reduceSum(function(d) {
+       if (d.Publisher === "Nintendo") {
+           return +d.Global_Sales;
+       } else {
+           return 0;
+       }
+
+   });
+
+   var sonysalesbyYear = yearDim.group().reduceSum(function(d) {
+       if (d.Publisher === "Sony Computer Entertainment") {
+           return +d.Global_Sales;
+       } else {
+           return 0;
+       }
+
+   });
+
+   var microsoftsalesbyYear = yearDim.group().reduceSum(function(d) {
+       if (d.Publisher === "Microsoft Game Studios") {
+           return +d.Global_Sales;
+       } else {
+           return 0;
+       }
+   });
+
    //Calculate metrics
-  
    var totalGlobalSales = yearDim.group().reduceSum(function (d) {
        return d["Global_Sales"];
    });
-
+   var TGS = ndx.groupAll().reduceSum(function (d) {
+       return d["Global_Sales"]
+   })
    var numByPlatform = platformDim.group()
-
+   var numByGenre = genreDim.group()
    var publisherGlobalSales = publisherDim.group().reduceSum(function (d) {
        return d["Global_Sales"];
    });
-
     var numByName = nameDim.group()
- 
- 
  
    //Define values (to be used in charts)
    var minyear = yearDim.bottom(1)[0]["Year"];
    var maxyear = yearDim.top(1)[0]["Year"];
  
    //Charts
-   var yearChart = dc.barChart("#time-chart");
-   var platformChart = dc.pieChart("#platform-chart")
-   var publisherChart = dc.barChart("#publisher-chart")
-   // var rankChart = dc.pieChart("#rank-chart")
-   // var vgNameND = dc.textDisplay("vg-Name-nd")
+    var yearChart = dc.barChart("#time-chart");
+    var platformChart = dc.pieChart("#platform-chart");
+    var publisherChart = dc.barChart("#publisher-chart");
+    var genreChart = dc.pieChart("#genre-chart");
+    var numbertotalSalesND = dc.numberDisplay("#number-totalsales-nd");
 
 selectField = dc.selectMenu('#menu-select')
-.dimension(nameDim)
-.group(numByName);
- 
+        .dimension(nameDim)
+        .group(numByName);
+
+    var topsalesChart = dc.compositeChart("#topsales-chart");
+
+numbertotalSalesND 
+    .formatNumber(d3.format("d"))
+    .valueAccessor(function (d) {
+        return d;
+    })
+    .group(TGS)
+    .formatNumber(d3.format(".3s"));
+
  yearChart
        .width(1200)
        .height(500)
@@ -67,9 +104,9 @@ selectField = dc.selectMenu('#menu-select')
        .transitionDuration(500)
        .x(d3.scale.ordinal())
        .xUnits(dc.units.ordinal)
-    //    .x(d3.time.scale().domain([minyear, maxyear]))
        .elasticY(true)
        .xAxisLabel("Year")
+       .yAxisLabel("Sales")
        .yAxis().ticks(10);
 
 platformChart
@@ -78,7 +115,21 @@ platformChart
        .radius(110)
        .transitionDuration(1500)
        .dimension(platformDim)
-       .group(numByPlatform);
+       .group(numByPlatform)
+       .cap(10) 
+       .minAngleForLabel(.10) 
+       .ordering( function(d) { return -5.0 * +d.value; });
+
+genreChart
+       .height(220)
+       .width(300)
+       .radius(110)
+       .transitionDuration(1500)
+       .dimension(genreDim)
+       .group(numByGenre)
+       .cap(10)
+       .minAngleForLabel(.30) 
+       .ordering( function(d) { return -5.0 * +d.value; });
 
 publisherChart
         .width(1200)
@@ -90,19 +141,31 @@ publisherChart
         .x(d3.scale.ordinal())
         .y(d3.scale.linear().domain([0,800]))
         .xUnits(dc.units.ordinal)
-        //    .x(d3.time.scale().domain([minyear, maxyear]))
         .elasticY(true)
-        .xAxisLabel("publisher")
+        .xAxisLabel("Publisher")
         .yAxis().ticks(10);
 
-// rankChart
-//        .height(220)
-//        .width(300)
-//        .radius(110)
-//        .transitionDuration(1500)
-//        .dimension(rankDim)
-//        .slicesCap()
-//        .group(numByRank);
+topsalesChart
+        .width(990)
+        .height(200)
+        .x(d3.time.scale().domain([minyear, maxyear]))
+        .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
+        .renderHorizontalGridLines(true)
+        .compose([
+            dc.lineChart(topsalesChart)
+                .dimension(yearDim)
+                .colors('green')
+                .group(ninsalesbyYear, 'Nintendo'),
+            dc.lineChart(topsalesChart)
+                .dimension(yearDim)
+                .colors('red')
+                .group(sonysalesbyYear, 'Sony'),
+            dc.lineChart(topsalesChart)
+                .dimension(yearDim)
+                .colors('blue')
+                .group(microsoftsalesbyYear, 'Microsoft')
+        ])
+        .brushOn(false);
 
  
 
